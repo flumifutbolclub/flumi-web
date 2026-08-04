068bf0d18dad7d5019baee6373b76c7fa8c800f3
flumifutbolclub <zj6tskngb6@privaterelay.appleid.com>
2026-08-04 02:16:44 -0300

    Actualizar layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,14 @@
+import "./globals.css";
+
 export const metadata = {
   title: "Flumi Fútbol Club",
-  description: "Sitio oficial de Flumi Fútbol Club",
+  description: "Sitio Oficial de Flumi Fútbol Club",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body
-        style={{
-          margin: 0,
-          padding: 0,
-          background: "#111",
-        }}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
